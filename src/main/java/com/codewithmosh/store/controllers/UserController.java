fc3953ab3234a5ecca107f2cf304c90d117c0b5e
vkhoma <khomavladimir@gmail.com>
2025-06-11 15:07:54 +0200

    Implemented validation

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -7,13 +7,17 @@ import com.codewithmosh.store.dtos.user.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
@@ -57,7 +61,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(
-            @RequestBody RegisterUserRequest request,
+            @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder
     ) {
         User user = userMapper.toEntity(request);
--- src/main/java/com/codewithmosh/store/dtos/user/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/user/RegisterUserRequest.java
@@ -1,12 +1,23 @@
 package com.codewithmosh.store.dtos.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 @Value
 public class RegisterUserRequest {
 
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
     String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 25, message = "Password must be between 6 to 25 characters long")
     String password;
 
 }
