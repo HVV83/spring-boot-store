98a29a9323b18e3cc02b0693fcd47d5c51964ad2
vkhoma <khomavladimir@gmail.com>
2025-07-03 17:58:46 +0200

    Set payment intent data for order
    Set payment intent data for order

--- src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
+++ src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
@@ -33,7 +33,7 @@ public class StripePaymentGateway implements PaymentGateway {
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("orderId", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntent(order));
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
@@ -46,6 +46,12 @@ public class StripePaymentGateway implements PaymentGateway {
             System.out.println(ex.getMessage());
             throw new PaymentException();
         }
+    }
+
+    private SessionCreateParams.PaymentIntentData createPaymentIntent(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", order.getId().toString())
+                .build();
     }
 
     @Override
